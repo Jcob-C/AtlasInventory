2e7a5dba6dc85896a4acca45049afb87a631c721
Jcob-C <arjhaycelestino54@email.com>
2025-05-08 07:19:20 +0800

    removed accounts module

--- src/main/java/delarama/DBC.java
+++
@@ -1,9 +0,0 @@
-package delarama;
-
-import java.sql.*;
-
-public class DBC {
-    public static Connection getConnection() throws SQLException {
-        return main.DB.getConnection();
-    }
-}
--- src/main/java/delarama/Panels.java
+++
@@ -1,9 +0,0 @@
-package delarama;
-
-public class Panels {
-    public static Account_Manager accountPanel = new Account_Manager();
-    public static Create createPanel = new Create();
-    public static UpDelete upDeletePanel = new UpDelete();
-    public static AuditTrail auditTrailPanel = new AuditTrail(); 
-
-}
